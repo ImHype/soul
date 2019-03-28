b956ffbe194664ef41e629c6ddc9d943876600a4
ImHype <543717080@qq.com>
2019-03-28 10:50:46 +0800

    feat: 010-container-with-most-water add better sol

--- leetcode/010-container-with-most-water.js
+++ leetcode/010-container-with-most-water.js
@@ -1,3 +1,4 @@
+// My Solution
 /**
  * @param {number[]} height
  * @return {number}
@@ -23,6 +24,32 @@ var maxArea = function(height) {
 
     return max;
 };
+
+// Better Soluter
+/**
+ * @param {number[]} height
+ * @return {number}
+ */
+var maxArea = function(height) {
+    let left = 0;
+    let right = height.length - 1;
+    let max = 0;
+
+    while (left < right) {
+        let step = right - left;
+        let h = Math.min(height[left], height[right]);
+        let area = step * h;
+
+        if (area > max) {
+            max = area;
+        } else {
+            (height[left] > height[right])? right-- : left++
+        }
+    }
+    
+    return max;
+};
+
 
 var height = [1,8,6,2,5,4,8,3,7];
 var result = maxArea(height);
